9d88823f84436552314b8e9a98a9f38e22ccd280
Coleman Collins <ccollins@thoughtworks.com>
2016-01-23 17:14:38 -0600

    weighted list

--- generator.js
+++ generator.js
@@ -1,4 +1,5 @@
 var Sentencer = require('sentencer');
+weightedComplimentList = [];
 
 Sentencer.configure({
   nounList: [],
@@ -15,20 +16,37 @@ Sentencer.configure({
   }
 });
 
-var compliments = [
-'You are the {{ superlative }} person in {{ location }}.',
-'DAYUM.',
-'<3',
-];
+//compliments                                            // weights
+var complimentList = {
+'You are the {{ superlative }} person in {{ location }}.' : 10,
+'DAYUM.'                                                  : 1,
+'<3'                                                      : 1,
+};
+
+var generateWeightedComplimentList = function() {
+  for (var compliment in complimentList) {
+    if (complimentList.hasOwnProperty(compliment)) {
+
+      //for each weight, push that many instances of the string into the weighted list
+      for (var i = 0; i < complimentList[compliment]; i++) {
+        weightedComplimentList.push(compliment);
+      }
+
+    };
+  };
+}
 
 var randomFromArray = function(array) {
   return array[Math.floor(Math.random() * array.length)];
-} 
+}
 
 var createCompliment = function() {
-  return Sentencer.make(randomFromArray(compliments));
+  return Sentencer.make(randomFromArray(weightedComplimentList));
 };
 
+//generate the compliment list
+generateWeightedComplimentList();
+//log a compliment into the terminal every second
 setInterval( function() {
   console.log(createCompliment());
 }, 1000);
